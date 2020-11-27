9afd545163e37d7a8baa4465a405ebaf8506ddf2
mariellevailla <marielle_34v@hotmail.com>
2020-11-27 11:40:13 -0500

    'test'Please enter the commit message for your changes. Lines starting 'test'test'test'test'test'test'test'test'test'test'test'test'test'''''''''''''#

--- monitoring_web_app/monitoring/views.py
+++ monitoring_web_app/monitoring/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<!DOCTYPE html> <html> <body> <h1> Room 800A </h1> <p>Temperature: 25C </p> <p>Humidity: 15% </p> <p>Pression 108 mmHg </p> </body> </html>')
 
+##Deuxième facon
+def home(request):
+    return render(request, 'monitoring/general_home.html')
--- monitoring_web_app/monitoring_web_app/settings.py
+++ monitoring_web_app/monitoring_web_app/settings.py
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
+## pas sur du os...
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
