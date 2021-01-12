f9d48fdaf61329a0e2258242759e20dc8c5c54a2
Émile Jomphe <hockey-expert_13@hotmail.com>
2021-01-12 14:25:59 -0500

    modified the views in the main app and the data visualization app

--- monitoring_web_app/monitoring/views.py
+++ monitoring_web_app/monitoring/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     return HttpResponse('<!DOCTYPE html> <html> <body> <h1> <a href="/test-room">Room 800A </a></h1> <p>Temperature: 25C </p> <p>Humidity: 15% </p> <p>Pression 108 mmHg </p> </body> </html>')
 
